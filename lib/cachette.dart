77f4df631947deb27a38cc14759ec391d105d3eb
Alex Baker <iamalexbaker@gmail.com>
2023-12-26 10:32:51 +0700

    Upgrade packages, use Result from elegant

--- lib/cachette.dart
+++ lib/cachette.dart
@@ -3,4 +3,3 @@ export 'src/constants.dart';
 export 'src/entry.dart';
 export 'src/entry_info.dart';
 export 'src/errors.dart';
-export 'src/result.dart';
--- lib/src/cachette.dart
+++ lib/src/cachette.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:cachette/cachette.dart';
+import 'package:elegant/elegant.dart';
 
 /// A simple in-memory cache.
 class Cachette<K, V extends Object> {
